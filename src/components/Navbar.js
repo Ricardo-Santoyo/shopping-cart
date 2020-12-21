3343b97d5dc3c9949d817819c65cd67b734be344
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-12-20 20:47:42 -0600

    added styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as BoxIcon } from './box-icon.svg';
 import { ReactComponent as CartIcon } from './shopping-cart.svg';
+import { cart } from './Cart';
 
 function Navbar() {
   return (
     <nav>
-      <h3>Logo</h3>
-
       <ul className="navlinks">
+        <Link to="/">
+          <li id="boxIcon"><BoxIcon /></li>
+        </Link>
+
         <Link to="/">
           <li>Home</li>
         </Link>
+
         <Link to="/shop">
           <li>Shop</li>
         </Link>
 
         <Link to="/cart">
-          <li id="cartIcon"><CartIcon /></li>
+          <li id="cartIcon"><CartIcon />
+            <span id="cartNumber">{cart.length}</span>
+          </li>
         </Link>
       </ul>
     </nav>
