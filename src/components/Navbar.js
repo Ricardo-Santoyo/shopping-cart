b360db3f5c5366ec7905d7066f603e387ae1c4b3
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-12-21 19:29:43 -0600

    removed navlink 'home'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,6 @@ function Navbar(props) {
           <li id="boxIcon"><BoxIcon /></li>
         </Link>
 
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-
         <Link to="/shop">
           <li>Shop</li>
         </Link>
