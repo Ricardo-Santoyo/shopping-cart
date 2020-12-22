5d08ca8f7850faba9581bcd8a0c31dc8069b17ce
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-12-21 21:41:41 -0600

    added images to shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Item from './Item';
-import Box from './images/cardboardbox.jpg'
+import Box from './images/cardboardbox.jpg';
+import Chair from './images/cardboardchair.jpg';
+import RoundTable from './images/cardboardroundtable.jpg';
+import Bench from './images/cardboardbench.jpg';
+import Sofa from './images/cardboardsofa.png';
+import BookShelf from './images/cardboardbookshelf.jpg';
+import Shoes from './images/cardboardshoes.jpeg';
+import Bag from './images/cardboardbag.jpeg';
+import Guitar from './images/cardboardguitar.jpg';
+import ClothesHanger from './images/cardboardclotheshanger.jpg';
 
 const items = [
-  {src:Box, name:'Cardboard Box', price:350},{src:Box, name:'Cardboard Box', price:350},
-  {src:Box, name:'Cardboard Box', price:350},{src:Box, name:'Cardboard Box', price:350},
-  {src:Box, name:'Cardboard Box', price:350},{src:Box, name:'Cardboard Box', price:350},
-  {src:Box, name:'Cardboard Box', price:350},{src:Box, name:'Cardboard Box', price:350},
-  {src:Box, name:'Cardboard Box', price:350},{src:Box, name:'Cardboard Box', price:350}
+  {src:Box, name:'Cardboard Box', price:350},{src:Chair, name:'Cardboard Chair', price:500},
+  {src:RoundTable, name:'Cardboard Round Table', price:600},{src:Bench, name:'Cardboard Bench', price:1250},
+  {src:Sofa, name:'Cardboard Sofa', price:2000},{src:BookShelf, name:'Cardboard Book Self', price:700},
+  {src:Shoes, name:'Cardboard Shoes', price:200},{src:Bag, name:'Cardboard Bag', price:50},
+  {src:Guitar, name:'Cardboard Guitar', price:800},{src:ClothesHanger, name:'Cardboard Clothes Hanger', price:40}
 ];
 
 function Shop() {
